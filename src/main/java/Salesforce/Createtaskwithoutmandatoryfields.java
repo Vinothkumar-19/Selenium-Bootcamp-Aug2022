e304ddceca65222738a6ebc91cd502c0f6ba8f4b
Vinothkumar <rajiivino@gmail.com>
2022-08-12 19:19:22 +0530

    second commit

--- src/main/java/Salesforce/Createtaskwithoutmandatoryfields.java
+++ src/main/java/Salesforce/Createtaskwithoutmandatoryfields.java
@@ -3,6 +3,8 @@ package Salesforce;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -26,6 +28,25 @@ public class Createtaskwithoutmandatoryfields {
 		driver.findElementByClassName("slds-r5").click();
 		driver.findElement(By.xpath("//lightning-button[@class='slds-button slds-p-horizontal--small']//button[1]")).click();
 		driver.findElement(By.xpath("//p[text()='Sales']")).click();
+		
+		 driver.findElement(By.xpath("//a[@title='Tasks']/following-sibling::one-app-nav-bar-item-dropdown/div/one-app-nav-bar-menu-button/a")).click();
+		  driver.findElement(By.xpath("//div[@class='menuItemsWrapper']/slot/one-app-nav-bar-menu-item")).click();
+		  
+		
+		   driver.findElement(By.xpath("(//div[@class='autocompleteWrapper slds-grow'])[2]")).click();
+		    driver.findElement(By.xpath("//div[@class='slds-m-left--smalllabels slds-truncate slds-media__body']/div")).click();
+		    driver.findElement(By.xpath("//textarea[contains(@class,'textarea textarea')]")).click();
+		    driver.findElement(By.xpath("//textarea[contains(@class,'textarea textarea')]")).sendKeys("SALES FORCE Automation Using Selenium");
+		    driver.findElement(By.xpath("//span[text()='Save']")).click();
+		    
+		    WebElement verify = driver.findElement(By.xpath("//li[text()='These required fields must be completed: Subject']"));
+		    if(verify.isDisplayed()) {
+	        	System.out.println("These required fields must be completed: Subject");
+	        }
+	        	else {
+	        		System.out.println("Testcase failed");
+	        	}
+					driver.close();
 	}
 
 }
